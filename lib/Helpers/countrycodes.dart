a5666824ec36d6e58222d1c7a397e172c84d9478
Advay Sanketi <115210355+AdvaySanketi@users.noreply.github.com>
2023-07-30 07:44:29 +0530

    Backend Core Files

---
+++ lib/Helpers/countrycodes.dart
@@ -0,0 +1,32 @@
+class ConstantCodes {
+  static const Map<String, String> countryCodes = {
+    'India': 'in',
+  };
+
+  // supported languages and their code
+  static const Map<String, String> languageCodes = {
+    'Belarusian': 'be',
+    'Chinese': 'zh',
+    'Czech': 'cs',
+    'Dutch': 'nl',
+    'English': 'en',
+    'French': 'fr',
+    'German': 'de',
+    'Hebrew': 'he',
+    'Hindi': 'hi',
+    'Hungarian': 'hu',
+    'Indonesian': 'id',
+    'Italian': 'it',
+    'Korean': 'ko',
+    'Malayalam': 'ml',
+    'Nepali': 'ne',
+    'Polish': 'pl',
+    'Portuguese': 'pt',
+    'Russian': 'ru',
+    'Spanish': 'es',
+    'Tamil': 'ta',
+    'Turkish': 'tr',
+    'Ukrainian': 'uk',
+    'Urdu': 'ur',
+  };
+}
